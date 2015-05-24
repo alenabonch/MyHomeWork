19ab2d6b5a0914bbaed81687e4fd3c12897ad10a
Alena <tiggy@mail.ru>
2015-05-24 10:50:31 +0300

    added roman numerals converter

---
+++ MyHomeWork/src/hw8/roman/RomanNumerals.java
@@ -0,0 +1,44 @@
+package hw8.roman;
+
+import java.util.LinkedHashMap;
+
+
+public class RomanNumerals {
+
+    public static void main(String[] args) {
+        System.out.println(convert(3999));
+    }
+
+    public static String convert(int number) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Integer key : lettersMap().keySet()) {
+            while (number >= key) {
+                sb.append(lettersMap().get(key));
+                number -= key;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static LinkedHashMap<Integer, String> lettersMap() {
+        LinkedHashMap<Integer, String> lettersMap = new LinkedHashMap<>();
+
+        lettersMap.put(1000, "M");
+        lettersMap.put(900, "CM");
+        lettersMap.put(500, "D");
+        lettersMap.put(400, "CD");
+        lettersMap.put(100, "C");
+        lettersMap.put(90, "XC");
+        lettersMap.put(50, "L");
+        lettersMap.put(40, "XL");
+        lettersMap.put(10, "X");
+        lettersMap.put(9, "IX");
+        lettersMap.put(5, "V");
+        lettersMap.put(4, "IV");
+        lettersMap.put(1, "I");
+
+        return lettersMap;
+    }
+
+}
---
+++ MyHomeWork/test/hw8/roman/RomanNumeralsTest.java
@@ -0,0 +1,49 @@
+package hw8.roman;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class RomanNumeralsTest {
+
+    private int input;
+    private String output;
+
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {1, "I"},
+                {4, "IV"},
+                {6, "VI"},
+                {13, "XIII"},
+                {39, "XXXIX"},
+                {44, "XLIV"},
+                {55, "LV"},
+                {78, "LXXVIII"},
+                {89, "LXXXIX"},
+                {100, "C"},
+                {1000, "M"},
+                {1055, "MLV"},
+        });
+    }
+
+    public RomanNumeralsTest(int input, String output) {
+        this.input = input;
+        this.output = output;
+    }
+
+
+    @Test
+    public void test() {
+        assertEquals(output, RomanNumerals.convert(input));
+    }
+
+}
